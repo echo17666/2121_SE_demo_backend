10a785432dead09daf219e1cb53582267b0eaccf
echo17666 <72566693+echo17666@users.noreply.github.com>
2023-03-11 16:55:45 +0800

    chore: 小改改user

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from user.models import User   
+from user.models import *   
 
 @csrf_exempt    
 def register(request):  
@@ -9,15 +9,13 @@ def register(request):
         username = request.POST.get('username') 
         password_1 = request.POST.get('password_1')
         password_2 = request.POST.get('password_2')
+        email = request.POST.get('email')
         if password_1 != password_2:   
-            return JsonResponse({'errno': 250, 'msg': "两次输入的密码不同"})
-        else:
-            if User.objects.filter(username=username).exists():  
-                return JsonResponse({'errno': 251, 'msg': "用户名已存在"})
-            else:
-                # 新建 Author 对象，赋值用户名和密码并保存
-                new_author = User(username=username, password=password_1)
-                new_author.save()   # 一定要save才能保存到数据库中
-                return JsonResponse({'errno': 200, 'msg': "注册成功"})
+            return JsonResponse({'errno': 250, 'msg': r"两次输入的密码不同"})
+        if User.objects.filter(username=username).exists():  
+            return JsonResponse({'errno': 251, 'msg': r"用户名已存在"})
+        user = User.objects.create(username=username, password=password_1,email=email)
+        user.save()
+        return JsonResponse({'errno': 200, 'msg': "注册成功"})
     else:
-        return JsonResponse({'errno': 252, 'msg': "请求方式错误"})
+        return JsonResponse({'errno': 255, 'msg': r"请求方式错误"})
