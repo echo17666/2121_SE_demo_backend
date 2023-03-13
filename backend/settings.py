3b2455f317caae626277a160a2c85e67d3759cbe
echo17666 <72566693+echo17666@users.noreply.github.com>
2023-03-13 17:19:30 +0800

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -150,6 +150,6 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 try:
-    from.local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
